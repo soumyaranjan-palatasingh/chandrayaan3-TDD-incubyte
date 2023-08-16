bb9bf5387e0a7f4c75d8d6773d85ff172d0a40ec
srpalatasingh <srpalatasingh@gmail.com>
2023-08-16 12:15:05 +0530

    Added basic rotataions like turn left, right, up and down

--- Chandrayan.py
+++ Chandrayan.py
@@ -30,3 +30,32 @@ class ChandrayanSpacecraft:
             self.position[2] -= 1
         elif self.direction == 'Down':
             self.position[2] += 1
+    
+    
+    def turn_left(self):
+        if self.direction == 'N':
+            self.direction = 'W'
+        elif self.direction == 'S':
+            self.direction = 'E'
+        elif self.direction == 'E':
+            self.direction = 'N'
+        elif self.direction == 'W':
+            self.direction = 'S'
+
+    def turn_right(self):
+        if self.direction == 'N':
+            self.direction = 'E'
+        elif self.direction == 'S':
+            self.direction = 'W'
+        elif self.direction == 'E':
+            self.direction = 'S'
+        elif self.direction == 'W':
+            self.direction = 'N'
+
+    def turn_up(self):
+        if self.direction == 'N' or self.direction == 'S':
+            self.direction = 'Up'
+
+    def turn_down(self):
+         if self.direction == 'N' or self.direction == 'S':
+            self.direction = 'Down'
