d81148ec19840e1d494cef6135ca1757284be445
srpalatasingh <srpalatasingh@gmail.com>
2023-08-16 12:12:34 +0530

    Added basic movements like move forward and backward

--- Chandrayan.py
+++ Chandrayan.py
@@ -2,3 +2,31 @@ class ChandrayanSpacecraft:
     def __init__(self, initial_position, initial_direction):
         self.position = initial_position
         self.direction = initial_direction
+        
+    def move_forward(self):
+        if self.direction == 'N':
+            self.position[1] += 1
+        elif self.direction == 'S':
+            self.position[1] -= 1
+        elif self.direction == 'E':
+            self.position[0] += 1
+        elif self.direction == 'W':
+            self.position[0] -= 1
+        elif self.direction == 'Up':
+            self.position[2] += 1
+        elif self.direction == 'Down':
+            self.position[2] -= 1
+
+    def move_backward(self):
+        if self.direction == 'N':
+            self.position[1] -= 1
+        elif self.direction == 'S':
+            self.position[1] += 1
+        elif self.direction == 'E':
+            self.position[0] -= 1
+        elif self.direction == 'W':
+            self.position[0] += 1
+        elif self.direction == 'Up':
+            self.position[2] -= 1
+        elif self.direction == 'Down':
+            self.position[2] += 1
